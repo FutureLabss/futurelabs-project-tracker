1331f6f6fccb33d6d5d94499ff5c67960687a40e
Usenmfon <usenmfonuko@gmail.com>
2026-09-02 16:21:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Futurelabs Project Tracker
+
+Scaffolded from the `project-track-demo` folder structure.
+
+Implementation code has intentionally not been added yet.
---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js';
+import babelParser from '@babel/eslint-parser';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default [
+  { ignores: ['dist', 'node_modules'] },
+  js.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: [
+            ['@babel/preset-react', { runtime: 'automatic' }],
+            '@babel/preset-typescript',
+          ],
+        },
+      },
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+    },
+  },
+];
